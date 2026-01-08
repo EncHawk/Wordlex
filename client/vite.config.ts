2cf93d9ac1ea7c6e60345444b42f63fdc1cdf6ba
D.Leap <dilipkumar2000.r@gmail.com>
2026-01-09 03:27:08 +0530

    all seems to work, well deployed. gotta do assignments now.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://wordlex-w5bw.onrender.com/",
         changeOrigin: true
       }
     }
